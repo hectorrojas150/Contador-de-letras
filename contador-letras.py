8ea5e705adfbe4346ba8a9fbc700a413c243149c
Hectorrojas150 <hectorrojas150@yahoo.com.ar>
2024-10-02 14:21:26 -0300

    realizamos un contador de palabras en python

---
+++ contador-letras.py
@@ -0,0 +1,13 @@
+#problematica: 
+#El usuario quiere saber cuantas letras tiene una palabra
+
+#Algoritmo
+#1) Solicitar al usuario que ingrese una palabra
+palabra_ingresada = input("por favor ingrese la palabra:")
+
+#2) Contar letras en la palabra ingresada
+cantidad_letras = len(palabra_ingresada)
+
+#3) Mostrar cantidad de letras que tiene la palabra ingresada
+print ("la palabra ingresada tiene", cantidad_letras , "letras")
+
